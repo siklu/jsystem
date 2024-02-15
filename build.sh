65979e3eccc2ca51f46bc34227edf2038c337aad
Moshe Libenson <moshe.l@siklu.com>
2024-02-15 13:43:17 +0200

    update build scripts

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
-mvn -f jsystem-parent/ clean install && mvn -f jsystem-assembly/jsystem-runner clean install
+mvn -f jsystem-parent/ clean package && mvn -f jsystem-assembly/jsystem-runner clean package
 if [ "$?" = "0" ]
 then
     if [ "x$1" = "xdeploy" ]
     then
-        mvn -f jsystem-assembly/jsystem-runner deploy
+        mvn -f jsystem-parent/ deploy && mvn -f jsystem-assembly/jsystem-runner deploy
     fi
 fi
